e27209903fd3353f577cc29edc7a7ce45861bf4d
Keisuke Watanuki <watanuki@ju-zen.com>
2023-12-11 07:34:33 +0900

    Add style and code format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  title: {
+    color: "#55c500",
+    fontSize: "5rem",
+    marginBlock: 0,
+  },
+  button: {
+    backgroundColor: "#ddd",
+  },
+  active: {
+    backgroundColor: "#212121",
+    color: "white",
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,9 +32,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 {...stylex.props(styles.title)}>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          {...stylex.props(count > 0 ? styles.active : styles.button)}
+        >
           count is {count}
         </button>
         <p>
@@ -29,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
